d94825d425488a71129dcf9a200dc34eb127f526
Swapnil Karki <swapnilk21030@gmail.com>
2019-05-15 11:42:29 +0545

    invoice icons

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -100,10 +100,10 @@
       <!-- accepted payments column -->
       <div class="col-6">
         <p class="lead">Payment Methods:</p>
-        <img src="../../dist/img/credit/visa.png" alt="Visa">
-        <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-        <img src="../../dist/img/credit/american-express.png" alt="American Express">
-        <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+        <img src="img/credit/visa.png" alt="Visa">
+        <img src="img/credit/mastercard.png" alt="Mastercard">
+        <img src="img/credit/american-express.png" alt="American Express">
+        <img src="img/credit/paypal.png" alt="Paypal">
 
         <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
           Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
